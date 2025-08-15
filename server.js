3391d585966791d2f9f5c45fc1fda05253498e01
ravi-64bit <107312602+ravi-64bit@users.noreply.github.com>
2025-08-15 19:35:04 +0530

    fix path public path issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 const SESSION_SECRET = process.env.SESSION_SECRET_KEY;
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
