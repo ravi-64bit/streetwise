01a8b903980aecedd08c245fb7d7dd0bbd142ebd
ravi-64bit <107312602+ravi-64bit@users.noreply.github.com>
2025-08-15 19:21:43 +0530

    fix vercel error

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const SESSION_SECRET = process.env.SESSION_SECRET_KEY;
 
 const app = express();
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
